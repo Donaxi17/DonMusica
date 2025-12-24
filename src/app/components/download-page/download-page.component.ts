aa4347f99cbef86f74e509740e3787c12624bced
Donaxi Jimenez <donaxjimenez00@gmail.com>
2025-12-23 23:16:45 -0500

    feat: add download page component to manage file, offline, and lyrics downloads with monetization and toast notifications.

--- src/app/components/download-page/download-page.component.ts
+++ src/app/components/download-page/download-page.component.ts
@@ -90,7 +90,7 @@ export class DownloadPageComponent implements OnInit, OnDestroy, AfterViewInit {
     this.isDownloading = true;
 
     // 1. Open Adsterra Smartlink (Monetization)
-    // window.open('https://www.effectivegatecpm.com/sw9g0tx52?key=973a1c8fac0e809dba93c52ce9b0de4c', '_blank');
+    window.open('https://www.effectivegatecpm.com/sw9g0tx52?key=973a1c8fac0e809dba93c52ce9b0de4c', '_blank');
 
     // 2. Now check if storage is full for offline mode
     if (this.mode === 'offline' && this.offlineService.isStorageFull()) {
